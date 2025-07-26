394a00ee4454e52aadeb9a929667719df1b8ddf4
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-07-26 13:35:38 +0400

    chore: correct errors committed earlier

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  alias: {
-      '@': path.resolve(__dirname, './src'),  // ← add
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-})
+  },
+});
